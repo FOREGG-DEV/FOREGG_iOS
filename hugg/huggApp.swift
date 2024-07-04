844606be46968add14aee95ae82a377673176718
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-07-04 13:24:23 +0900

    Initial Commit

---
+++ hugg/huggApp.swift
@@ -0,0 +1,17 @@
+//
+//  huggApp.swift
+//  hugg
+//
+//  Created by Donghan Kim on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct huggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
